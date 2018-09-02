0cf5fce993f4e997d07a941437c60b61852edef6
Nathan Pinsker <npinsker@gmail.com>
2018-09-02 01:05:54 -0700

    activate cats & more fixups

--- source/WorldConstants.hx
+++ source/WorldConstants.hx
@@ -4,12 +4,14 @@ import openfl.utils.Object;
 
 class WorldConstants {
 	public static var shrineLocationMap:Array<Object> =
-		[ {tx: 4, ty: 3, x: 5, y: 3, id: "shrine1"},
-		  {tx: 5, ty: 3, x: 7, y: 5, id: "shrine2"},
-		  {tx: 5, ty: 5, x: 7, y: 6, id: "shrine3"},
-		  {tx: 4, ty: 1, x: 4, y: 4, id: "shrine4"},
-		  {tx: 2, ty: 3, x: 4, y: 4, id: "shrine5"},
-		  {tx: 2, ty: 2, x: 4, y: 4, id: "shrine6"}];
+		[ {tx: 4, ty: 3, x: 6, y: 6, id: "shrine1"},
+		  {tx: 2, ty: 4, x: 6, y: 3, id: "shrine2"},
+		  {tx: 3, ty: 1, x: 4, y: 3, id: "shrine3"},
+		  {tx: 2, ty: 5, x: 6, y: 4, id: "shrine4"},
+		  {tx: 1, ty: 2, x: 4, y: 5, id: "shrine5"},
+		  {tx: 5, ty: 5, x: 4, y: 5, id: "shrine6"},
+		  {tx: 0, ty: 0, x: 7, y: 5, id: "shrine7"},
+		  {tx: 5, ty: 0, x: 7, y: 2, id: "shrine8"}];
 		  
 	public static var shrineInfo:Map<String, Object> =
 		["shrine1" => {tx_start: 0, ty_start: 2, x_start: 4, y_start: 9},
@@ -23,7 +25,11 @@ class WorldConstants {
 	
 	public static var specialTileTypes:Map<Int, String> =
 		[ 67 => "crate", 91 => "playerCrate", 296 => "cannon",
-		  336 => "cat", 337 => "cat", 338 => "cat", 360 => "cat", 361 => "cat", 362 => "cat" ];
+		  336 => "cat", 337 => "cat", 338 => "cat", 360 => "cat", 361 => "cat", 362 => "cat",
+		  339 => "cat", 340 => "cat", 341 => "cat", 342 => "cat", 343 => "cat", 344 => "cat",
+		  345 => "cat", 346 => "cat", 347 => "cat", 348 => "cat", 363 => "cat", 364 => "cat",
+		  626 => "cat", 627 => "cat", 628 => "cat", 672 => "cat", 673 => "cat", 674 => "cat",
+		  675 => "cat", 676 => "cat"];
 	
 	public static var shrinesWithPermanentGates:Array<String> = ["shrine5"];
 	
